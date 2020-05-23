323c310869f3e8acbace88e483d65fd6661c11d5
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2020-05-23 14:38:21 +0200

    more episodes again. done with e111 now.

--- lesson107-109(messages_for_dicts).py
+++ lesson107-109(messages_for_dicts).py
@@ -1,5 +1,6 @@
 #https://www.youtube.com/watch?v=Rii4QJuXrlQ&list=PL6lxxT7IdTxFKo9DguLxGM2dhgb8-u976&index=107
 
+### Metoder for dict class ###
 
 #EPISODE 107
     #pop() tar ut valgt key/value pair. Man kan også sette verdien av dette parret til en variabel 
@@ -63,3 +64,26 @@ print(returned_value)
 print(list(returned_value))
 print() # for plass
 
+#EPISODE 111
+    #get()
+    #vi kan bruke get() hvis vi ikke vil skrive en kode med try/exept keywords 
+    #hvis get() ikke finner argumentet, returnerer det "none"
+    #hvis man ikke bruker noen av disse, vil man få en error når man ikke har key'en man leter etter.
+    
+    
+mine_aksjer = {
+    "akso":6.25,
+    "mowi":165,
+    "bakkafrost":605,
+    }
+
+try:
+    print(mine_aksjer["akso"])
+    print(mine_aksjer["SalMar"])
+except KeyError:
+    print("SalMar is not here")
+    
+print() #for plass
+
+print(mine_aksjer.get("akso"))
+print(mine_aksjer.get("SalMar"))
